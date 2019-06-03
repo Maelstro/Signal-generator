37d5904d7c8ac125a79c85b7616c7d890cfd9757
Maciej Wojcik <maciej.wojcik.xperia@gmail.com>
2019-06-03 18:00:23 +0200

    Finished FFT generator
    Signed-off-by: Maciej Wojcik <maciej.wojcik.xperia@gmail.com>

--- time-to-frequency.py
+++ time-to-frequency.py
@@ -1,7 +1,7 @@
 #Generates FFT from file
 
 import numpy as np
-import scipy as s
+import scipy as sc
 import tkinter as tk
 import matplotlib.pyplot as plt
 import matplotlib
@@ -19,9 +19,21 @@ class Window(tk.Tk):
         frame.grid(row=0, column=0, sticky='nsew')
         menu = tk.Menu(frame)
         menu.add_command(label="Open file", command=self.calculate)
-        menu.add_command(label="Help", command=lambda: self.help_popup)
+        menu.add_command(label="Help", command=self.help_popup)
         self.config(menu=menu)
         self.protocol("WM_DELETE_WINDOW", self.callback)
+        # Define figure
+        frame.canvasFig = plt.figure(1)
+        fig = matplotlib.figure.Figure(dpi=100)
+        fig_subplot = fig.add_subplot(111)
+        self.line1, = fig_subplot.plot(0, 0, 'r-')
+        self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=frame)
+        self.plot_widget = self.canvas.get_tk_widget()
+        tk.Grid.columnconfigure(frame, 1, weight=1)
+        tk.Grid.rowconfigure(frame, 0, weight=1)
+        self.plot_widget.grid(row=0, column=1, rowspan=2, sticky="nsew")
+        self.update()
+
 
     def callback(self):
         if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -35,9 +47,20 @@ class Window(tk.Tk):
         transformed = np.fft.fft(signal, fsHz)/fsHz
         magTransformed = abs(transformed)
         faxis = np.linspace(-fsHz/2, fsHz/2, fsHz)
-        plt.plot(faxis, np.fft.fftshift(magTransformed))
-        plt.grid()
-        plt.show()
+        finalFFT = np.fft.fftshift(magTransformed)
+        plt.plot(faxis, finalFFT)
+        self.refresh_fig(faxis, finalFFT)
+
+    def refresh_fig(self, x, y):
+        self.line1.set_data(x, y)
+        ax = self.canvas.figure.axes[0]
+        ax.set_xlim(x.min(), x.max())
+        ax.set_ylim(y.min() - 1, y.max() + 1)
+        ax.grid(self)
+        self.canvas.draw()
+
+    def help_popup(self):
+        tk.messagebox.showinfo("Help", "Window in creation...")
 
 if __name__ == '__main__':
     app = Window()
